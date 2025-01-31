72ffaf0e3382e4214cbc92c430aea6c6e91a52d3
biyash-1 <sthaalish39@gmail.com>
2025-01-31 22:55:28 +0545

    ui updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-// pages/mypage.tsx
-
 "use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import TodaysSpecial from "@/components/Todays";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
+  const services = [
+    {
+      image: "/delivery.jpg",
+      title: "Fast Delivery",
+      description: "Get your food delivered to your doorstep in record time. We prioritize speed without compromising quality.",
+    },
+    {
+      image: "/easypayment.jpg",
+      title: "Easy Payment Methods",
+      description: "Enjoy hassle-free payments with multiple options, including credit/debit cards, digital wallets, and cash on delivery.",
+    },
+    {
+      image: "/ordertracking.jpg",
+      title: "Real-Time Order Tracking",
+      description: "Track your order in real-time from the restaurant to your location. Know exactly when your food will arrive.",
+    },
+  ];
+
   return (
     <>
       <div className="min-h-[90vh] flex flex-col justify-center items-center pt-16 md:pt-0">
@@ -20,16 +37,35 @@ export default function Home() {
             <Button>Explore now</Button>
           </div>
           <div className="flex justify-center mt-2 md:mt-0">
-            <Image src={'/burger2.png'} height={400} width={350} alt="Burger Image" />
+            <Image src={"/burger2.png"} height={400} width={350} alt="Burger Image" />
           </div>
         </section>
       </div>
 
-     
       <TodaysSpecial />
 
-      <section className="cheif h-screen mx-auto py-4">
-        <h1 className="text-center text-2xl font-semibold">Our Chief chef</h1>
+      <section className="cheif h-screen mx-auto mt-12 py-7">
+        <h1 className="text-center text-2xl font-semibold mb-8">Our Services</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center px-4">
+          {services.map((service, index) => (
+            <Card key={index} className="w-full max-w-[300px] h-[400px] flex flex-col  justify-between shadow-lg hover:shadow-xl transition-shadow duration-300">
+              <div className="relative h-[300px] w-full">
+                <Image
+                  src={service.image}
+                  alt={service.title}
+                  layout="fill"
+                  
+                  objectFit="cover"
+                  className="rounded-t-lg"
+                />
+              </div>
+              <CardHeader>
+                <CardTitle className="text-xl font-bold ">{service.title}</CardTitle>
+                <CardDescription className="text-gray-600">{service.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
       </section>
     </>
   );
