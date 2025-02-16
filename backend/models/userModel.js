54f66565898a15dbd5fcbc27ed649b4bdd44dd16
biyash-1 <sthaalish39@gmail.com>
2025-02-16 23:07:27 +0545

    feild added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,31 @@
-import  mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-
-const userSchema  = new Schema ({
-    username:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
-
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    role:{
-        type:String,
-        enum:['admin','user'],
-        default:'user'
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    refreshToken: {
+        type: String,  // Store the latest valid refresh token
+        default: null
     }
-  
-})
+});
 
-export default mongoose.model('User',userSchema)
+export default mongoose.model('User', userSchema);
